0304007c9c6d4c475248b2f0f46647d463b8e27e
Bárbara Lima <Brbara Lima>
2016-06-28 21:30:36 -0300

    Modificando ver causa

--- app/src/main/java/com/example/samsung/linben/VerCausaActivity.java
+++ app/src/main/java/com/example/samsung/linben/VerCausaActivity.java
@@ -2,10 +2,13 @@ package com.example.samsung.linben;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class VerCausaActivity extends AppCompatActivity {
 
